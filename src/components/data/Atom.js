b1ebedfb3cb9f16eccc3bb6c9e894dfae680f238
DESKTOP-2ACSP4E\gkstn <gkstn784512@naver.com>
2022-12-18 15:54:58 +0900

    s

--- src/components/data/Atom.js
+++ src/components/data/Atom.js
@@ -30,30 +30,25 @@ export const RowChangeData = atom({
 });
 
 export const Row = atom([
-  { id: 1, title: "가나다", user: "lbm1212", description: "하늘은왜" },
-  { id: 2, title: "라마바", user: "jky2323", description: "아아아아아아" },
+  { id: 1, title: "배송 관련 문의", user: "wogud3", description: "상품 주문이 5일 지났는데 배송은 얼마나 걸리나요?" },
+  { id: 2, title: "상품교환 문의", user: "ssswss", description: "옷 사이즈가 작아서 한치수 크게 교환하려합니다" },
   {
     id: 3,
-    title: "사아",
+    title: "교환",
     user: "dlaguswns12",
-    description: "ㅜㅜㅜㅜㅜㅜㅜㅜㅜㅜ"
+    description: "배송되면서 옷이 찢어졌습니다 교환부탁드려요",
   },
   {
     id: 4,
-    title: "차카타",
-    user: "qudtlsdlaguswns12",
-    description: "??????????????"
+    title: "환불문의",
+    user: "dwadwad",
+    description: "배송이 너무 늦어 환불요청드립니다",
   },
   {
     id: 5,
-    title: "파하다",
-    user: "guswnszns",
-    description: "나나나나나나나나ㅏ나"
+    title: "재입고 문의",
+    user: "qudtn2321",
+    description: "라이트 그레이 105 재입고는 언제쯤 되나요?",
   },
-  {
-    id: 6,
-    title: "히히히",
-    user: "dkflzkxh",
-    description: "아니아니아니아니아니아니아니아니아니아니"
-  }
+
 ]);
